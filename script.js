0de40d99d09c6a91710b80c42d7afe993c529e99
Kamela Williamson <kami63116@yahoo.com>
2020-04-21 13:02:48 -0700

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 //drills 1
 
 //object assignments
+
+function createMyObject() {
+    return {
+      foo: 'bar',
+      answerToUniverse: 42,
+      'olly olly': 'oxen free',
+      sayHello: function() {
+        return 'hello';
+      },
+    };
+  }
+
+  //object updater
+
+  function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
+  }
